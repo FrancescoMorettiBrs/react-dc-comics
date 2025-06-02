d3b3d81b7c9eace641d95336e9a8dce52abb8390
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2025-06-03 00:24:06 +0200

    feat: footer & task completed

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,5 +1,157 @@
 function AppFooter() {
-    return <footer>Sono il footer</footer>
+  return (
+    <footer className="footer">
+      <div className="row-footer">
+        <ul>
+          <a href="">
+            <li>
+              <img src="/buy-comics-digital-comics.png" alt="" />
+              <p>DIGITAL COMICS</p>
+            </li>
+          </a>
+          <a href="">
+            <li>
+              <img src="/buy-comics-merchandise.png" alt="" />
+              <p>DC MERCHANDISE</p>
+            </li>
+          </a>
+          <a href="">
+            <li>
+              <img src="/buy-comics-subscriptions.png" alt="" />
+              <p>SUBSCRIPTION</p>
+            </li>
+          </a>
+          <a href="">
+            <li>
+              <img src="/buy-comics-shop-locator.png" alt="" />
+              <p>COMIC SHOP LOCATOR</p>
+            </li>
+          </a>
+          <a href="">
+            <li>
+              <img src="/buy-dc-power-visa.svg" alt="" />
+              <p>DC POWER VISA</p>
+            </li>
+          </a>
+        </ul>
+      </div>
+      <div className="bg-footer">
+        <div class="footer-links">
+          <div class="footer-column">
+            <h3>DC COMICS</h3>
+            <ul>
+              <li>
+                <a href="#">Characters</a>
+              </li>
+              <li>
+                <a href="#">Comics</a>
+              </li>
+              <li>
+                <a href="#">Movies</a>
+              </li>
+              <li>
+                <a href="#">TV</a>
+              </li>
+              <li>
+                <a href="#">Games</a>
+              </li>
+              <li>
+                <a href="#">Videos</a>
+              </li>
+              <li>
+                <a href="#">News</a>
+              </li>
+            </ul>
+            <h3>SHOP</h3>
+            <ul>
+              <li>
+                <a href="#">Shop DC</a>
+              </li>
+              <li>
+                <a href="#">Shop DC Collectibles</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="footer-column">
+            <h3>DC</h3>
+            <ul>
+              <li>
+                <a href="#">Terms Of Use</a>
+              </li>
+              <li>
+                <a href="#">Privacy policy (New)</a>
+              </li>
+              <li>
+                <a href="#">Ad Choices</a>
+              </li>
+              <li>
+                <a href="#">Advertising</a>
+              </li>
+              <li>
+                <a href="#">Jobs</a>
+              </li>
+              <li>
+                <a href="#">Subscriptions</a>
+              </li>
+              <li>
+                <a href="#">Talent Workshops</a>
+              </li>
+              <li>
+                <a href="#">CPSC Certificates</a>
+              </li>
+              <li>
+                <a href="#">Ratings</a>
+              </li>
+              <li>
+                <a href="#">Shop Help</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="footer-column">
+            <h3>SITES</h3>
+            <ul>
+              <li>
+                <a href="#">DC</a>
+              </li>
+              <li>
+                <a href="#">MAD Magazine</a>
+              </li>
+              <li>
+                <a href="#">DC Kids</a>
+              </li>
+              <li>
+                <a href="#">DC Universe</a>
+              </li>
+              <li>
+                <a href="#">DC Power Visa</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="footer-logo">
+        <img src="/dc-logo-bg.png" alt="" />
+      </div>
+      <div className="bg-second-footer">
+        <div>
+          <button>SIGN-UP NOW!</button>
+        </div>
+        <div className="icons">
+          <p>FOLLOW US</p>
+          <img src="/footer-facebook.png" alt="" />
+          <img src="/footer-twitter.png" alt="" />
+          <img src="/footer-youtube.png" alt="" />
+          <img src="/footer-pinterest.png" alt="" />
+          <img src="/footer-periscope.png" alt="" />
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default AppFooter;
